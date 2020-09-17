0e9fd5673b52978e4914f56f84fbd06e1312dfaa
Still34 <Still34@users.noreply.github.com>
2020-09-17 15:26:35 +0000

    jekyll build from Action 0fc68101a438fe12c115c0c8bb5a9bf0ab504914

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,73 @@
+var store = [{
+        "title": "AIS3 2019 Ransomware Sample Analysis",
+        "excerpt":"Dynamic analysis Dynamic analysis is used here first because it is already actively running on victim’s machine, so we might as well start from there. Discovering malicious executable Launch Process Hacker on the victim VM; an anomaly named 5D85C2C17D.exe can be found in the process list. The process is launched...","categories": ["writeup"],
+        "tags": ["en-us"],
+        "url": "https://stillu.cc/writeup/2019/07/31/ais3-2019-ransomware-sample-analysis/",
+        "teaser": null
+      },{
+        "title": "Capture the Nitro Writeup",
+        "excerpt":"So the story goes like this: a few weeks ago my friend took notice that I was playing CTF. He got curious and decided to make a little CTF challenge specifically for me. The prize? One month Nitro Classic on Discord. I was like sure, sounds like fun. For the...","categories": ["writeup"],
+        "tags": ["en-us"],
+        "url": "https://stillu.cc/writeup/2020/06/23/capture-the-nitro/",
+        "teaser": null
+      },{
+        "title": "AIS3 2020 Pre-exam Writeup (中文)",
+        "excerpt":"前言 小弟中文不好，如果有中文不通順的地方請見諒 &lt;(＿　＿)&gt; 這次比我去年第一次打的時候有經驗多了，去年差不多100名左右，這次爬到了 43 名。很可惜的，有幾題我都是戳中邊或者是花太多時間在部分題目導致其他幾題沒有多餘時間看完。比如像是 Clara，Clara 那題我很早就看到有問題的封包甚至是 Key Exchange 中的 Key，但是只能說經驗不足，一開始沒意識到我所看到的字串部分是 Key，而加密方式是純粹 XOR。我是在比賽結束後沒多久才發現這回事的。非常可惜因為 Clara 那題 500 分。但整體而言我還是對這次 AIS3 很滿意也覺得又學到了不少，如 Rose 那題，我在之前其實沒有意識到 Python 在執行時會 Compile 成 Bytecode，也趁機學會了這點。 Web 🐿 Squirrel (100 pt.) 這題一打開會看到一堆到處在跑的松鼠，一開始完全不知道在幹嘛，但那些松鼠上寫的用戶名非常似曾相識… 這些不是常見的Linux使用者帳號嗎？ 估計是從 /etc/passwd 翻出來的，咱們來看看這個網頁的原始碼。 ❯ curl https://squirrel.ais3.org/ &lt;!DOCTYPE html&gt; &lt;head&gt; &lt;title&gt;🐿️🐿️🐿️🐿️🐿️&lt;/title&gt; &lt;meta charset=\"utf-8\"&gt; &lt;link rel=\"stylesheet\" href=\"css/style.css\"&gt; &lt;/head&gt;...","categories": ["writeup"],
+        "tags": ["zh-tw"],
+        "url": "https://stillu.cc/writeup/2020/06/28/ais3-preexam-zh-2020/",
+        "teaser": null
+      },{
+        "title": "The Current State of the Internet and its Absurdity",
+        "excerpt":"Today I was sorting through the extensions I’ve been using over the past few months. There’s the standard bunch like uBlock Origin, Dark Reader, Stylish, and whatnot. You know, something that everyone might have in their browser. What about the rest? The rest are mostly “privacy-focused” extensions: Decentraleyes, Privacy Badger,...","categories": ["rambling"],
+        "tags": ["en-us"],
+        "url": "https://stillu.cc/rambling/2020/06/29/internet-and-its-absurdity/",
+        "teaser": null
+      },{
+        "title": "Strays and Morality",
+        "excerpt":"2AM. Awaken by the itching of my overly sensitive skin. Tried to fall back to sleep, alas, all efforts were in vain. Stomach growled. 3AM, defeated, I biked to the nearest McDonald’s to grab a quick meal; the meal was nothing more than just a cheap chicken sandwich and a...","categories": ["rambling"],
+        "tags": ["philosophy","en-us"],
+        "url": "https://stillu.cc/rambling/2020/06/29/strays-and-morality/",
+        "teaser": null
+      },{
+        "title": "Struggling with Gender Dysphoria",
+        "excerpt":"Junior high was the darkest time of my life. I was bullied heavily at school because of my personal hygiene (or lack thereof) and various family-related issues. My family had always fought over one thing or another. I had an abusive pedo girlfriend who was much older than I was....","categories": ["rambling"],
+        "tags": ["mental-health","en-us"],
+        "url": "https://stillu.cc/rambling/2020/07/10/struggling-with-gender-dysphoria/",
+        "teaser": null
+      },{
+        "title": "rgbCTF 2020 Writeup - Part 1",
+        "excerpt":"Prologue I was not planning on participating any CTF this week, but I figured I’d check the CTFTime schedule few days ago. That was when I saw a CTF named “rgbCTF” was coming up within a few days. I was like “screw it, let’s go, why not?” My interest peaked...","categories": ["writeup"],
+        "tags": ["en-us"],
+        "url": "https://stillu.cc/writeup/rgbctf-2020-part-1",
+        "teaser": null
+      },{
+        "title": "rgbCTF 2020 Writeup - Part 2",
+        "excerpt":"Prologue This is second part of the rgbCTF writeup, and contains information about challenges solved by me, Emzi. I strongly suggest starting with the first part of this writeup. This was my first CTF event, however not the first set of challenges I solved. As I’m new, the methods outlined...","categories": ["writeup"],
+        "tags": ["en-us"],
+        "url": "https://stillu.cc/writeup/rgbctf-2020-part-2",
+        "teaser": null
+      },{
+        "title": "A Closer Look at CAPA",
+        "excerpt":"What is it While looking around Fireeye’s website for the latest news on their Flare-on CTF, I found that they had just recently released a new tool for threat researchers, dubbed capa. I thought it’d be a perfect opportunity to further expand ever-growing collection of Windows Security Tools. So what...","categories": ["infosec"],
+        "tags": ["en-us"],
+        "url": "https://stillu.cc/infosec/2020/07/19/closer-look-at-capa/",
+        "teaser": null
+      },{
+        "title": "Windows Security Tools",
+        "excerpt":"Reverse Engineering Use multiple tools for comparison when decompiling! Every tool creates different results. Always use multiple decompilers if you encounter weird results. Generic Binaries Ghidra ✅ Pricing: Free ✅ Codebase: Open-source 📝 Notes: Made by NSA Binary Ninja ⚠ Pricing: Paid ($149 USD/$4,500 NTD) ⚠ Codebase: Proprietary 📝 Notes:...","categories": ["infosec"],
+        "tags": ["en-us"],
+        "url": "https://stillu.cc/windows-security-tools",
+        "teaser": null
+      },{
+        "title": "Humanity and Sentimental Value of Songs",
+        "excerpt":"When I fell in love with Avicii’s work right around the time Level was released, I once swore that progressive house would be my only favorite genre of music and would listen to nothing else. I had a special relationship with the genre after all; it was the genre that...","categories": ["rambling"],
+        "tags": ["philosophy","en-us"],
+        "url": "https://stillu.cc/rambling/2020/08/03/humanity-songs/",
+        "teaser": null
+      },{
+        "title": "Threat Spotlight - DLL Hijacking (and .NET)",
+        "excerpt":"Welcome to a new series I’m calling Threat Spotlight! In this series, we will focus on common vulnerabilities or potential risk factors that developers or security researchers should consider when securing their systems. In this article, we’ll be focusing on the way Windows handles DLL loading, how threat actors may...","categories": ["threat-spotlight"],
+        "tags": ["en-us","dotnet","guide"],
+        "url": "https://stillu.cc/threat-spotlight/2020/09/16/dll-hijacking/",
+        "teaser": null
+      }]
